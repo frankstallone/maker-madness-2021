0c770bd2dab7ce5356256dc68fa29d98dca24739
Larry Boyer <larryboyer@Larrys-MacBook-Pro.local>
2021-04-23 00:59:21 -0400

    Adding T8M #6

--- src/pages/TakeMake/TakeMake06.js
+++ src/pages/TakeMake/TakeMake06.js
@@ -1,13 +1,8 @@
 import React from 'react';
-// import img01 from '../../img/tnm04/tnm04-01.png';
-// import img02 from '../../img/tnm04/tnm04-02.png';
-// import img03 from '../../img/tnm04/tnm04-03.png';
-// import img04 from '../../img/tnm04/tnm04-04.png';
-// import img05 from '../../img/tnm04/tnm04-05.png';
-// import img06 from '../../img/tnm04/tnm04-06.png';
-// import img07 from '../../img/tnm04/tnm04-07.png';
-// import img08 from '../../img/tnm04/tnm04-08.png';
-// import img09 from '../../img/tnm04/tnm04-09.png';
+
+import img01 from '../../img/tnm06/Maplewoodshop_logo.png';
+import img02 from '../../img/tnm06/mask_rack.jpeg';
+
 import PageHero from '../../components/PageHero';
 import PageTemplate from '../../components/PageTemplate';
 
@@ -19,7 +14,18 @@ const TakeMake06 = () => {
         mainHeader="Mask Rack / Bracelet Rack"
       />
       <PageTemplate>
-        <h3>(Content coming soon)</h3>
+        <a href="https://www.maplewoodshop.com">
+          <img className="mx-auto w-56" src={img01} alt="Step" />
+        </a>
+
+        <p><strong>Step 1</strong> - Sand any rough edges using the sandpaper.</p>
+        <p><strong>Step 2</strong> - Set the board on a flat surface like a table.</p>
+        <p><strong>Step 3</strong> - Gently twist and push the thinner dowel through the board.</p>
+        <p><strong>Step 4</strong> - Feel underneath to make sure the end of the dowel and the board are flush (at the same level).</p>
+        <p><strong>Step 5</strong> - Gently twist the larger dowel onto the thinner dowel until it too is flush with the top of the thinner dowel.</p>
+        <img className="mx-auto" src={img02} alt="Step" />
+
+        <p>If your rack is slipping, you can use a small amount of glue to connect any of the pieces.</p>
       </PageTemplate>
     </>
   );
