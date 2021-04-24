7c6616c2dd6693af7afb6ad29f1d2c42f9399fc7
Larry Boyer <larryboyer@Larrys-MacBook-Pro.local>
2021-04-24 03:59:48 -0400

    Updates to Fidget Spinner.

--- src/pages/OYO.js
+++ src/pages/OYO.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+
+import img01 from '../img/oyo/fll.png';
+
 import PageHero from '../components/PageHero';
 import PageTemplate from '../components/PageTemplate';
 const OYO = () => {
@@ -12,6 +15,7 @@ const OYO = () => {
       />
       <PageTemplate>
         <p>The following “On Your Own (OYO)” projects were were put together by the MAPSO MAKERS, a FIRST® LEGO® League robotics team made up of 8th grade girls from South Orange Middle School. All of these projects should be able to be completed with materials around the house, including what’s in your recycling bins!</p>
+
         <ul>
           <li>
             <NavLink to={`/oyo01`}>OYO #1 - Fidget Spinner</NavLink>
@@ -30,6 +34,15 @@ const OYO = () => {
             <NavLink to={`/takemake05`}>OYO #5 - Balloon Races</NavLink>
           </li>
         </ul>
+
+        <a href="https://www.firstlegoleague.org">
+          <img className="mx-auto" src={img01} alt="First Lego League" />
+        </a>
+        <p>
+If you are a K-8 student in the SOMSD community and have interest in coding, robotics, community projects or just playing with LEGO bricks, you may want to learn more!</p>
+        <p>
+This community team runs outside of the school system by Maplewood dad Brad Schenker and SOMS middle school choir teacher Jake Ezzo. Email MAPSOFLL@gmail.com for more information!</p>
+
       </PageTemplate>
     </>
   );
--- src/pages/OYO/OYO01.js
+++ src/pages/OYO/OYO01.js
@@ -1,13 +1,8 @@
 import React from 'react';
-// import img01 from '../../img/tnm04/tnm04-01.png';
-// import img02 from '../../img/tnm04/tnm04-02.png';
-// import img03 from '../../img/tnm04/tnm04-03.png';
-// import img04 from '../../img/tnm04/tnm04-04.png';
-// import img05 from '../../img/tnm04/tnm04-05.png';
-// import img06 from '../../img/tnm04/tnm04-06.png';
-// import img07 from '../../img/tnm04/tnm04-07.png';
-// import img08 from '../../img/tnm04/tnm04-08.png';
-// import img09 from '../../img/tnm04/tnm04-09.png';
+
+import img01 from '../../img/oyo/img1.png';
+import img02 from '../../img/oyo/img2.png';
+
 import PageHero from '../../components/PageHero';
 import PageTemplate from '../../components/PageTemplate';
 
@@ -16,7 +11,36 @@ const OYO01 = () => {
     <>
       <PageHero supHeader="OYO - OnYourOwn #1" mainHeader="Fidget Spinner" />
       <PageTemplate>
-        <h3>(Content coming soon)</h3>
+        <h3>Recycled Fidget Spinner Project</h3>
+        <h5>Created by Alexa Garrido</h5>
+        <p>(Parental supervision suggested)</p>
+
+
+        <h3>Materials</h3>
+        <div className="px-8 bg-yellow-50 border-2 border-yellow-200 rounded-lg">
+          <ul>
+            <li>Cardboard</li>
+            <li>Two plastic bottle caps •One lollipop stick</li>
+            <li>Scissors</li>
+            <li>Hot glue</li>
+            <li>One lollipop stick</li>
+            <li>Colored marker/crayon or colored paper</li>
+          </ul>
+        </div>
+
+        <h4>Instructions</h4>
+        <p><strong>Step 1</strong> - Gather all the materials and get to a clean workspace.</p>
+        <img className="mx-auto" src={img02} alt="Fidget Spinner Template" />
+        <p><strong>Step 2</strong> - Using one of the three templates, cut out a fidget spinner shape out of cardboard.</p>
+        <p><strong>Step 3</strong> - With parental supervision, poke a hole in the middle of the shape you just cut out with the scissors, and try not to make the hole too big, as the lollipop stick is going to fit in it.</p>
+        <p><strong>Step 4</strong> - If you have a marker, or crayon, you can color both sides of the cardboard shape. If you have colored paper instead, then cut two pieces of paper that are the exact same shape as the cardboard and poke a hole in the same place as the hole in the cardboard.</p>
+        <p><strong>Step 5</strong> - If you are using crayons or markers, please skip this step. Glue the colored paper on either side of the cardboard, and if you need to fix the hole, please do so.</p>
+        <p><strong>Step 6</strong> - Now, poke the lollipop stick through the cardboard, and cut it so about a half of an inch is poking from either side of the cardboard.</p>
+        <p><strong>Step 7</strong> - Now, with parental supervision, glue each end of the lollipop stick to the inside middle of a plastic water bottle cap.  </p>
+        <p><strong>Step 8</strong> - Now once everything is glued and dried, put one finger on each bottle cap, and spin, and enjoy!</p>
+
+        <img className="mx-auto" src={img01} alt="Fidget Spinner" />
+
       </PageTemplate>
     </>
   );
